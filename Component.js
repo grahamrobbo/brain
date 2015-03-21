875cedaf247f512d61c0c600d80c7a8a03e93cfa
Graham Robinson <graham@yelcho.com.au>
2015-03-21 17:26:14 +1100

    Change height

--- Component.js
+++ Component.js
@@ -5,5 +5,5 @@ jQuery.sap.declare("samples.components.brain.Component");
 sap.ui.core.UIComponent.extend("samples.components.brain.Component", {});
 
 samples.components.brain.Component.prototype.createContent = function() {
-    return new sap.m.Image({src: "img/brain.jpg"});
+    return new sap.m.Image({src: "img/brain.jpg", height:"400px"});
 };
