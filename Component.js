45938b4489ece983e9522645977aafc59f402fb2
Graham Robinson <graham@yelcho.com.au>
2015-03-21 16:39:08 +1100

    Initial commit

---
+++ Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.require("sap.ui.core.UIComponent");
+jQuery.sap.require("sap.m.Image");
+jQuery.sap.declare("samples.components.brain.Component");
+
+sap.ui.core.UIComponent.extend("samples.components.brain.Component", {});
+
+samples.components.brain.Component.prototype.createContent = function() {
+    return new sap.m.Image({src: "img/brain.jpg"});
+};
